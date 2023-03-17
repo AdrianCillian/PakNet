af0b733fdabd35e99aa06d5ecca1754258b18655
Adrian Cillian Ansel <126966433+AdrianCillian@users.noreply.github.com>
2023-03-17 12:33:31 +0200

    Create PakNetHandlerSource.cpp

---
+++ src/functions/PakNetHandlerSource.cpp
@@ -0,0 +1,81 @@
+// PakNetFunctions.cpp
+
+#include "PakNetFunctions.h"
+
+// Network-Related PakNet Functions
+
+void pak::SendPacket() {
+    // implementation
+}
+
+void pak::SendNewHttp() {
+    // implementation
+}
+
+void pak::CloseNewHttp() {
+    // implementation
+}
+
+void pak::DelayNewHttp() {
+    // implementation
+}
+
+void pak::GetNewHttp() {
+    // implementation
+}
+
+void pak::EnableHttpPacket() {
+    // implementation
+}
+
+void pak::EnablePacket() {
+    // implementation
+}
+
+// Class Editing-Related PakNet Functions (Static Library)
+
+void pak::NewClass() {
+    // implementation
+}
+
+void pak::ExitClass() {
+    // implementation
+}
+
+void pak::NewClassCoroutine() {
+    // implementation
+}
+
+void pak::CloseClassCoroutine() {
+    // implementation
+}
+
+void pak::NewGlobalClass() {
+    // implementation
+}
+
+void pak::ExitGlobalClass() {
+    // implementation
+}
+
+void pak::NewGlobalClassCoroutine() {
+    // implementation
+}
+
+void pak::CloseGlobalClassCoroutine() {
+    // implementation
+}
+
+// Function Handler-Related Paknet Functions (Static Library)
+
+void pak::HandleNewFunction() {
+    // implementation
+}
+
+void pak::AssignHandleMainClass() {
+    // implementation
+}
+
+void pak::AssignNewGlobalMainClass() {
+    // implementation
+}
